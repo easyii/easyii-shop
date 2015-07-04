1a2b4584155312af56b145ce946f7cb392880e54
linhbm <manhlinh18@gmail.com>
2015-07-04 11:30:13 +0700

    first easyii

---
+++ app/config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=easyii-shop',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'tablePrefix' => '',
+    'enableSchemaCache' => true,
+];
